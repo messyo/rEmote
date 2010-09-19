8400ecc81394d32d25be303cbc511f75a5ecb893
Matthias Mayr <messyo@cssbook.de>
2010-09-19 22:06:09 +0200

    Improved previous workaround

--- remote/filedetails.php
+++ remote/filedetails.php
@@ -453,8 +453,9 @@ if(isset($_GET['action']) && isset($actionsarr[$_GET['action']]))
 				// The following clean code could not be used due to a bug in PHP...
 				// http://bugs.php.net/bug.php?id=45028
 				// $intHash = hash_file('crc32b', $path);
+				//
 				if(is_valid_file($path) && is_readable($path))
-					$intHash = dechex(crc32(file_get_contents($path)));
+					$intHash = str_pad(dechex(crc32(file_get_contents($path))), 8, '0', STR_PAD_LEFT);
 				else
 					$intHash = '';
 
