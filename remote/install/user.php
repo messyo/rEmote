a30b9409f96eff7994f5725752c553b58c123ce1
Matthias Mayr <messyo@cssbook.de>
2010-10-28 23:02:44 +0200

    Fixed problem in Installer

--- remote/install/user.php
+++ remote/install/user.php
@@ -36,7 +36,7 @@ if(isset($_POST['save']))
 		$salt = makeRandomStr(10, false);
 		$count = intval($db->one_result($db->query('SELECT COUNT(*) AS c FROM users'), 'c'));
 		if($count == 0)
-			$db->query('INSERT INTO users (name, password, salt, status, dir, rootdir, viewchange, sortord, refchange, viewmode, groupmode, sourcemode, sortkey, refinterval, refmode, detailsstyle, language, design, sidebar, detailsmode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+			$db->query('INSERT INTO users (name, password, salt, status, dir, rootdir, viewchange, sortord, refchange, viewmode, groupmode, sourcemode, sortkey, refinterval, refmode, detailsstyle, language, design, detailsmode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
 				'sssissisiiiiiiiissi',
 				$v_name,
 				sha1($v_pass.$salt),
@@ -56,7 +56,6 @@ if(isset($_POST['save']))
 				2,
 				$_SESSION['language'],
 				$db->one_result($db->query('SELECT value FROM settings WHERE skey = ?', 's', 'default_style'), 'value'),
-				0,
 				0);
 	}
 }
