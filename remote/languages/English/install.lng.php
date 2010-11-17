7d7db6a5e4c301bd3bbd59f990176cdaa479b038
Matthias Mayr <messyo@cssbook.de>
2010-11-17 21:13:47 +0100

    Recovered Wrtlprnfts improvements to the english language. Let installer check for sysvsem and session extension.

--- remote/languages/English/install.lng.php
+++ remote/languages/English/install.lng.php
@@ -8,33 +8,33 @@ $lng['chooselng']       = 'Please choose your language';
 $lng['save']            = 'Save';
 $lng['coulntread']      = 'FATAL: Could not read File (\\1)';
 
-$lng['checkrt']         = 'Check for rTorrent-Connection';
-$lng['insecurerpc']     = 'You are Using an Insecure RPC-Connection';
-$lng['insechint']       = 'Secure your connection by using an unguessable Path, like "\\1" (this Path is generated on random, so feel free to use it. Or secure your connection with authentication by user and password.';
-$lng['insechint']      .= 'Do not Forget to change the SCGI-Mount-Command in your httpd.conf accordingly (e.g. \\2)';
-$lng['insechint']      .= '<br />You also could secure your connection, by binding your RPC-Path to a specific IP-Address in your webservers configuration, to avoid unauthorized access.';
+$lng['checkrt']         = 'Check connection to rTorrent';
+$lng['insecurerpc']     = 'You are using an insecure RPC connection';
+$lng['insechint']       = 'Secure your connection by using an unguessable path, like "\\1" (this path is generated on random, so feel free to use it. Or secure your connection with authentication by user and password.';
+$lng['insechint']      .= 'Do not Forget to change the SCGI mount command in your httpd.conf accordingly (e.g. \\2)';
+$lng['insechint']      .= '<br />You also could secure your connection, by binding your RPC path to a specific IP-Address in your webserver\'s configuration, to avoid unauthorized access.';
 $lng['noconnect']       = 'Could not connect to rTorrent, please ensure rTorrent is started';
 $lng['rtfound']         = 'Found rtorrent with Version \\1';
-$lng['rtwrongvers']     = 'This rTorrent-Version is not supported, please use rtorrent in version \\1 or higher';
+$lng['rtwrongvers']     = 'This rTorrent version is not supported, please use rTorrent version \\1 or higher';
 $lng['retry']           = 'Retry';
 
 
 $lng['database']        = 'Database';
-$lng['connected']       = 'Successfully connected to Database';
-$lng['couldntcon']      = 'Could not connect to Databse with following parameters: \\1';
-$lng['dbtype']          = 'Database-Type: \\1';
+$lng['connected']       = 'Successfully connected to database';
+$lng['couldntcon']      = 'Could not connect to database with the following parameters: \\1';
+$lng['dbtype']          = 'Database type: \\1';
 $lng['dbdb']            = 'Database: \\1';
 $lng['dbhost']          = 'Host: \\1';
 $lng['dbuser']          = 'User: \\1';
 $lng['dbpass']          = 'Password: \\1';
-$lng['incomplete']      = 'Connection-Parameters incomplete';
-$lng['uptodate']        = 'All Tables up to date';
+$lng['incomplete']      = 'Connection parameters incomplete';
+$lng['uptodate']        = 'All tables up to date';
 
-$lng['firstuser']       = 'Creating First User';
-$lng['userfound']       = 'Found User';
-$lng['invname']         = 'Invalid Username';
-$lng['invpass']         = 'Invalid Password';
-$lng['invdir']          = 'Invalid Direcotry';
+$lng['firstuser']       = 'Creating first user';
+$lng['userfound']       = 'Found user';
+$lng['invname']         = 'Invalid username';
+$lng['invpass']         = 'Invalid password';
+$lng['invdir']          = 'Invalid direcotry';
 $lng['nodir']           = 'No such directory';
 $lng['username']        = 'Username';
 $lng['password']        = 'Password';
@@ -44,8 +44,8 @@ $lng['settings']        = 'Settings';
 $lng['adding']          = 'Adding option "\\1"';
 $lng['deleting']        = 'Deleting option "\\1"';
 $lng['settingsu2d']     = 'All settings are up to date';
-$lng['newsettings']     = 'New options have been added. Go to the settings menu in your Control-Panel after the installation to adjust them.';
-$lng['errorins']        = 'An error has occured while inserting new Options';
+$lng['newsettings']     = 'New options have been added. Go to the settings menu in your control panel after the installation to adjust them.';
+$lng['errorins']        = 'An error has occured while inserting new options';
 
 
 
@@ -59,13 +59,13 @@ $lng['executable']      = 'Executable';
 $lng['not']             = 'not';
 $lng['pass']            = 'Passed';
 $lng['function']        = 'Function';
-$lng['tmpdir']          = 'Temporary Files';
-$lng['default_dir']     = 'Default Home-Directory';
-$lng['user_dir']        = 'First Users Directory';
-$lng['ppassed']         = 'Some functions may not work (mostly Filebrowser-Functions)';
+$lng['tmpdir']          = 'Temporary files';
+$lng['default_dir']     = 'Default home directory';
+$lng['user_dir']        = 'First user\'s directory';
+$lng['ppassed']         = 'Some functions may not work (mostly filebrowser-Functions)';
 $lng['pbad']            = 'There are more permissions needed to continue installation';
 $lng['pgood']           = 'The directory has the neccessary attributes';
-$lng['changedir']       = 'Change Directories';
+$lng['changedir']       = 'Change directories';
 
 $lng['binaries']        = 'Programs';
 $lng['bpassed']         = 'Some functions may not work';
@@ -74,11 +74,11 @@ $lng['bgood']           = 'All requirements satisfied';
 $lng['binary']          = 'Program';
 $lng['bnotfound']       = 'Not Found';
 $lng['autodet']         = 'Autodetection';
-$lng['changebin']       = 'Change Pathes';
+$lng['changebin']       = 'Change paths';
 
 $lng['completion']      = 'Completion';
-$lng['comsuccess']      = 'The installer has been successfully locked. <strong>The Installation is now complete.</strong> You can proceed logging into rEmote and creating other Users and adjust your settings';
-$lng['notlocked']       = 'The installer could not be locked. Please make sure the installer has the permission to create the file ".lock" or the file is created and writable by the rEmote-Installer';
+$lng['comsuccess']      = 'The installer has been successfully locked. <strong>The installation is now complete.</strong> You can proceed by logging into rEmote and creating other users and adjusting your settings';
+$lng['notlocked']       = 'The installer could not be locked. Please make sure the installer has the permission to create the file ".lock" or the file is created and writable by the rEmote installer';
 $lng['notlockedi']      = 'You can also delete the install-folder to complete the installation.';
 $lng['goremote']        = 'Go to rEmote';
 
@@ -88,25 +88,27 @@ $lng['yes']             = 'Yes';
 $lng['no']              = 'No';
 $lng['reqd']            = 'Required';
 $lng['curr']            = 'Current';
-$lng['apachemods']      = 'Apache Modules';
-$lng['phpextens']       = 'PHP-Extensions';
+$lng['apachemods']      = 'Apache modules';
+$lng['phpextens']       = 'PHP extensions';
 $lng['notava']          = 'Not available';
 
-$lng['mod_scgi']        = 'Apache-Module SCGI';
-$lng['xmlrpc']          = 'PHP-Extension XMLRPC';
-$lng['gd']              = 'PHP-Extension GDLib';
-$lng['xml']             = 'PHP-Extension XML';
-$lng['PDO']             = 'PHP-Extension PDO';
-$lng['pdo_mysql']       = 'PDO-Driver for MySQL';
-$lng['pdo_pgsql']       = 'PDO-Driver for PostgreSQL';
-$lng['pdo_sqlite']      = 'PDO-Driver for SQLite';
-$lng['pdo_oci']         = 'PDO-Driver for Oracle';
-$lng['mysql']           = 'PHP-Extension MySQL';
+$lng['mod_scgi']        = 'Apache module SCGI';
+$lng['xmlrpc']          = 'PHP extension XMLRPC';
+$lng['gd']              = 'PHP extension GDLib';
+$lng['xml']             = 'PHP extension XML';
+$lng['PDO']             = 'PHP extension PDO';
+$lng['pdo_mysql']       = 'PDO driver for MySQL';
+$lng['pdo_pgsql']       = 'PDO driver for PostgreSQL';
+$lng['pdo_sqlite']      = 'PDO driver for SQLite';
+$lng['pdo_oci']         = 'PDO driver for Oracle';
+$lng['mysql']           = 'PHP extension MySQL';
+$lng['sysvsem']         = 'PHP extension for Semaphore-Support';
+$lng['session']         = 'PHP extension for Sessions';
 $lng['on']              = 'On';
 $lng['off']             = 'Off';
 $lng['safe_mode']       = 'Safe Mode';
 $lng['open_basedir']    = 'Open Basedir Restriction';
 $lng['allow_url_fopen'] = 'Allow URL fopen';
-$lng['dbinvalid']       = 'Invalid database-type';
+$lng['dbinvalid']       = 'Invalid database type';
 
 ?>
